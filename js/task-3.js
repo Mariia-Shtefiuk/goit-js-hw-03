90f811dd7db208ce46b181edb59a496e9659921d
Mariia Shtefiuk <masha.shtf@gmail.com>
2025-04-20 19:02:27 +0300

    js folder update

--- js/task-3.js
+++ js/task-3.js
@@ -1,11 +1,25 @@
-function getElementWidth(content, padding, border) {
-  const contentWidth = parseFloat(content);
-  const paddingWidth = parseFloat(padding) * 2;
-  const borderWidth = parseFloat(border) * 2;
-  const containerWidth = contentWidth + paddingWidth + borderWidth;
-  return containerWidth;
+// function filterArray(numbers, value) {
+//   const newArray = [];
+//   for (let i = 0; i < numbers.length; i += 1) {
+//     if (numbers[i] > value) {
+//       newArray.push(numbers[i]);
+//     }
+//   }
+//   return newArray;
+// }
+
+function filterArray(numbers, value) {
+  const newArray = [];
+  for (const number of numbers) {
+    if (number > value) {
+      newArray.push(number);
+    }
+  }
+  return newArray;
 }
 
-console.log(getElementWidth('50px', '8px', '4px'));
-console.log(getElementWidth('60px', '12px', '8.5px'));
-console.log(getElementWidth('200px', '0px', '0px'));
+console.log(filterArray([1, 2, 3, 4, 5], 3));
+console.log(filterArray([1, 2, 3, 4, 5], 4));
+console.log(filterArray([1, 2, 3, 4, 5], 5));
+console.log(filterArray([12, 24, 8, 41, 76], 38));
+console.log(filterArray([12, 24, 8, 41, 76], 20));
